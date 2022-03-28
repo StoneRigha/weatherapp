6829bac26e647cbd42467bf115899060cd4f2611
Livingstone Maghanga <stoneriga@gmail.com>
2022-03-28 18:31:10 +0300

    setting up the api key

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" name="" id="" class="searh-bar"/>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
@@ -7,8 +13,10 @@
 
 export default {
   name: 'App',
-  components: {
-    
+  data() {
+    return{
+      api_key:'6e6a3abfade7babcc6bcd7f89d5f1f7c'
+    }
   }
 }
 </script>
